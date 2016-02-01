cb9ffa3d62c9555d1e468a60bcda2eb2a656bc53
kelvingamedev <kelvin.gamedeveloper@gmail.com>
2016-02-01 20:26:13 -0200

    Aula2

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float Speed = 10f;
+    public KeyCode MoveUp, MoveDown;
+   
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKey(MoveUp))
+        {
+           gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, Speed);
+
+        }
+
+        else if(Input.GetKey(MoveDown))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, Speed * -1);
+
+        }
+
+        else
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+        }
+    }
+}
