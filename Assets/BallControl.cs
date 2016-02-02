11fc905886300c545079c694b18f3a22ea082acf
kelvingamedev <kelvin.gamedeveloper@gmail.com>
2016-02-02 20:23:10 -0200

    Checkpoint - Erro "default gameobject tag player already registered"

---
+++ Assets/BallControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour {
+
+    public float XForce , YForce ;
+   
+
+	// Use this for initialization
+	void Start () {
+        float randNumber = Random.Range(0f, 1f);
+        if(randNumber <= 0.5f )
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(XForce, YForce));
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(XForce, YForce));
+        }
+    }
+	
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.CompareTag("Player"))
+        {
+            Debug.Log("It's working");
+        }
+    }
+}
