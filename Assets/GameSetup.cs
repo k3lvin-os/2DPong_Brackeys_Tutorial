adf6c6492be7be9ee47e5e39ccba97ed072f9302
kelvingamedev <kelvin.gamedeveloper@gmail.com>
2016-02-03 17:23:44 -0200

    Aula5

--- Assets/GameSetup.cs
+++ Assets/GameSetup.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameSetup : MonoBehaviour {
+public class GameSetup : MonoBehaviour
+{
 
     public BoxCollider2D TopWall, BottomWall, LeftWall, RightWall;
     public Transform Player1, Player2;
     public Camera MainCamera;
-    private float playerDistEdgeX = 1.5f;
 
     // In begin of scene
-	void Start () {
-
+    void Start()
+    {
+        float playerDistEdgeX = 1.5f;
         float rightEdge = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x;
         float leftEdge = MainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x;
 
@@ -21,13 +22,13 @@ public class GameSetup : MonoBehaviour {
         BottomWall.size = new Vector2(MainCamera.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f);
         BottomWall.offset = new Vector2(0f, MainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y - 0.5f);
 
-        LeftWall.size = new Vector2(1f, MainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y );
-        LeftWall.offset = new Vector2(MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
-
         RightWall.size = new Vector2(1f, MainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y);
-        RightWall.offset = new Vector2(MainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
+        RightWall.offset = new Vector2(MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, 0f)).x + 0.5f, 0f);
 
-        Player1.position = new Vector3(leftEdge + playerDistEdgeX,0f,0f);
+        LeftWall.size = new Vector2(1f, MainCamera.ScreenToWorldPoint(new Vector3(0f, Screen.height * 2f, 0f)).y);
+        LeftWall.offset = new Vector2(MainCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).x - 0.5f, 0f);
+
+        Player1.position = new Vector3(leftEdge + playerDistEdgeX, 0f, 0f);
         Player2.position = new Vector3(rightEdge - playerDistEdgeX, 0f, 0f);
 
     }
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static int playerOneScore = 0;
+    public static int playerTwoScore = 0;
+    public GUISkin myGUiSkin;
+
+    public static void Score(string wallName)
+    {
+        if (wallName.Equals("rightWall"))
+        {
+            playerOneScore++;
+        }
+        else
+        {
+            playerTwoScore++;
+        }
+
+        Debug.Log("Player1 score is " + playerOneScore);
+        Debug.Log("Player2 score is " + playerTwoScore);
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = myGUiSkin;
+        GUI.Label(new Rect(Screen.width / 2 - 200, 20, 100, 100), "" + playerOneScore);
+        GUI.Label(new Rect(Screen.width / 2 + 200, 20, 100, 100), "" + playerTwoScore);
+
+    }
+}
